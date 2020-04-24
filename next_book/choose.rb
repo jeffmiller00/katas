7bef8f098fd4fc708a189b8048eef2466ba3547c
Jeff Miller <jmiller@ncsasports.org>
2020-04-24 11:58:36 -0500

    Randomly choose a weighted sample.

--- next_book/choose.rb
+++ next_book/choose.rb
@@ -6,21 +6,22 @@ open(url) do |rss|
   feed = RSS::Parser.parse(rss)
   puts "Title: #{feed.channel.title}"
   puts "|--------------------------------------------------"
-  puts "Random read is: #{feed.items.sample.title}"
+  # puts "Random read is: #{feed.items.sample.title}"
   weighted_list = []
   feed.items.each do |item|
     weight = (item.description.split('rating:')[1].split('book published:')[0].to_f * 100).round
     weighted_list << Array.new(weight, item.title)
   end
-  puts "Weighted random read is: #{weighted_list.flatten.sample}"
+  # puts "Weighted random read is: #{weighted_list.flatten.sample}"
 
-  puts "|--------------------------------------------------"
-  while true
-    weighted = weighted_list.flatten.sample
-    random   = feed.items.sample.title
-    if weighted == random
-      puts "The random weighted book is: #{weighted} | #{random}"
-      break
-    end
+  i = 0
+  random = feed.items.sample.title
+  weighted_random = weighted_list.flatten.sample
+  while random != weighted_random
+    random = feed.items.sample.title
+    weighted_random = weighted_list.flatten.sample
+    i += 1
+    puts "Attempt: #{i}"
   end
+  puts "#{random} | #{weighted_random}"
 end
